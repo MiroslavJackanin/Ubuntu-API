b2019391950b09bf3e2ece3905e69fbda9d850f3
Miroslav Jackanin <miroslav.jackanin@akademiasovy.sk>
2021-03-20 17:50:48 +0100

    init commit

---
+++ src/main/java/sk/it/demo/DemoController.java
@@ -0,0 +1,13 @@
+package sk.it.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @RequestMapping("/")
+    public String demo() {
+        return "Hello world!";
+    }
+}
