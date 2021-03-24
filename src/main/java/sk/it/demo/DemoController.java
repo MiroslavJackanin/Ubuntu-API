80bb855f38997b59fffeb85e96313f0b27f942b7
MiroslavJackanin <miroslav.jackanin@kosickaakademia.sk>
2021-03-24 11:40:38 +0100

    changed mappings

--- src/main/java/sk/it/demo/DemoController.java
+++ src/main/java/sk/it/demo/DemoController.java
@@ -4,9 +4,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/test")
 public class DemoController {
 
-    @RequestMapping("/")
+    @RequestMapping("/api")
     public String demo() {
         return "Hello world!";
     }
